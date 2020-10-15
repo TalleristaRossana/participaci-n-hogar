e58b40280ffcefa92d7769e66bf47694c21cfc89
Rossana96 <rossana@ciencias.unam.mx>
2020-10-14 20:37:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var sumMujeres = 0;
-var sumHombres = 0;
-
-function calculate(){
-  if(document.getElementById('mujer1').checked){
-    sumMujeres++;
+function calculate() {
+  var sumMujeres = 0;
+  var sumHombres = 0;
+  var buttonM, elemM, buttonH, elemH;
+  for (i=1; i<5; i++) {
+    buttonM = 'mujer'+i;
+    elemM = document.getElementById(buttonM);
+    buttonH = 'hombre' + i;
+    elemH = document.getElementById(buttonH);
+    if (elemM.checked == true) { sumMujeres += Number(elemM.value); }
+    if (elemH.checked == true) { sumHombres += Number(elemH.value); }
   }
-  else if(document.getElementById('hombre1').checked){
-    sumHombres++;
-  }
-
-  var divobj = document.getElementById('total');
-  divobj.value = sumMujeres;
+  console.log('Labores realizadas por mujeres: ' + sumMujeres + ', Labores realizadas por hombres: ' + sumHombres);
 }
